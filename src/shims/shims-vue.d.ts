09873d25ecbfefb8516b009da999b1e94235d6b7
Qwertronix <qwertronix@gmail.com>
2020-02-08 02:45:50 -0500

    refactor(shims-vue): add lodash `$_` vue proto def to type shims

--- src/shims/shims-vue.d.ts
+++ src/shims/shims-vue.d.ts
@@ -1,9 +1,11 @@
 import Vue from 'vue'
+import { LoDashStatic } from 'lodash';
 
 declare module 'vue/types/vue' {
   interface Vue {
     $appVersion: string
     $lancerVersion: string
+    $_: LoDashStatic
     $platform: string
     $notify: (text: string, type?: string, onClick?: () => void) => void
   }
